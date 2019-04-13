92f064ed17ce5b3ed75e67a046d86282594a0886
Jake Swartwout <43890936+JakeSwartwout@users.noreply.github.com>
2019-04-13 13:35:24 -0600

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -37,6 +37,8 @@ class Graph
     void readInWord(string word, string previous);
     //exactly the same as readInWord, except that it increments it by a count, rather than by 1
     void readInSave(string word, string previous, int count);
+    //writes the graph to a file
+    void saveToFile(string fileName);
     //prints all of the nodes and all of their connections
     void displayEdges();
     //returns a word predicted from the word passed in
