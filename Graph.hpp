03ae003664dc4299729f1102ae96a3ac12df0a1f
Jake Swartwout <43890936+JakeSwartwout@users.noreply.github.com>
2019-04-13 12:22:24 -0600

    Upload Graph.hpp

---
+++ Graph.hpp
@@ -0,0 +1,50 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <vector>
+#include <iostream>
+
+struct vertex;
+
+/*This is the struct for the adjacent vertices for each
+vertex in the graph. */
+struct Edge
+{
+    vertex *v;
+    int distance;
+};
+
+/*this is the struct for each vertex in the graph. */
+struct vertex
+{
+    string name;
+    vector<Edge> Edges; //stores edges to adjacent vertices
+};
+
+struct trieNode{
+
+};
+
+class Graph
+{
+  public:
+    Graph();
+    ~Graph();
+    //either increases the word's frequency or creates a new one and connects it
+    void readInWord(string word, string previous);
+    //creates a new vertex with the word
+    void addWord(string word);
+    //creates a connection or increases the count between the two words
+    void createConnection(string word1, string word2); //maybe change these to vertex pointers?
+    //prints all of the nodes and all of their connections
+    void displayEdges();
+
+  private:
+    //the vertices in a list form, for easy looping
+    std::vector<vertex> vertices; //stores vertices
+
+    vertex *findVertex(std::string name);
+
+};
+
+#endif // GRAPH_HPP
